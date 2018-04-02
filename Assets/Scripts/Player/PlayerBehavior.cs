8a15c2b72abd46c857a3146bb53bb22e96a9b55d
LonelyZach <zach@lonelydevelopment.com>
2018-04-01 20:28:56 -0400

    Basic server autoritative RPC commands.

---
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerBehavior : NetworkBehaviour
+{
+  void Update()
+  {
+    if (!isLocalPlayer)
+    {
+      return;
+    }
+
+    if (Input.GetKeyDown(KeyCode.S))
+    {
+      CmdMoveDown();
+    }
+    else if (Input.GetKeyDown(KeyCode.A))
+    {
+      CmdMoveLeft();
+    }
+    else if (Input.GetKeyDown(KeyCode.D))
+    {
+      CmdMoveRight();
+    }
+  }
+
+  [Command]
+  void CmdMoveLeft()
+  {
+    gameObject.transform.Translate(new Vector3(-1, 0, 0));
+  }
+
+  [Command]
+  void CmdMoveRight()
+  {
+    gameObject.transform.Translate(new Vector3(1, 0, 0));
+  }
+
+  [Command]
+  void CmdMoveDown()
+  {
+    gameObject.transform.Translate(new Vector3(0, -1, 0));
+  }
+}
